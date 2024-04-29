5525b281196149886016d350ae9aa29821a09b8f
Ростислав <kingartes1989@gmail.com>
2024-04-29 18:12:24 +0300

    Build fix

--- Assets/PlayerAttackAnimator.cs
+++ Assets/PlayerAttackAnimator.cs
@@ -1,12 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEditor.Animations;
 using UnityEngine;
 
 public class PlayerAttackAnimator : MonoBehaviour
 {
     private Animator animator;
-    private AnimatorState shootingState;
 
     private void Awake()
     {
@@ -16,16 +14,10 @@ public class PlayerAttackAnimator : MonoBehaviour
 
     public void PauseAttackAnimation()
     {
-        Debug.Log("Pause");
-        AnimatorController ac = animator.runtimeAnimatorController as AnimatorController;
-        shootingState = ac.layers[1].stateMachine.states[1].state;
-        Debug.Log(shootingState);
-        shootingState.speed = 0;
+
     }
 
     public void ResumeAttackAnimation()
     {
-        Debug.Log("RESUME");
-        animator.SetTrigger("Shootfinished");
     }
 }
